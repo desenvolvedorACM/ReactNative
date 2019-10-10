2ffd9366526c1881580aa396414cd62c4e268a3e
Desenvolvedoracm <desenvolvedoracm@gmail.com>
2019-10-10 17:59:04 -0300

    add to project

---
+++ AppBiscoito/src/index.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  AlertAndroid
+} from 'react-native';
+
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 20
+  },
+  textoFrase: {
+    color: '#dd7b22',
+    fontStyle: 'italic',
+    fontSize: 20,
+    margin: 30,
+    textAlign: 'center'
+  },
+  img: {
+    width: 300,
+    height: 300
+  }
+});
+
+const img = require('../assets/biscoito.png');
+
+class Botao extends Component {
+  constructor(props) {
+    super(props);
+
+    this.styles = StyleSheet.create({
+      botao: {
+        width: 370,
+        height: 50,
+        backgroundColor: '#dd7b22',
+        borderRadius: 25
+      },
+      btnArea: {
+        flex: 1,
+        flexDirection: `row`,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      btnTexto: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: props.cor
+      }
+    });
+  }
+
+  render() {
+    return (
+      <TouchableOpacity style={this.styles.botao} onPress={ this.props.onClick }>
+        <View style={this.styles.btnArea}>
+          <Text style={this.styles.btnTexto}>Gerar Frase</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+}
+
+class App extends Component {
+  constructor(props){
+     super(props);
+
+     this.state = {
+       textoFrase: '',
+       error: ''
+     }
+
+     this.geraFrase = this.geraFrase.bind();
+     this.carregaFrases();
+  }
+  
+  carregaFrases = () => {
+    this.frases = [
+      'Duas coisas são infinitas: o universo e a estupidez humana. Mas, em relação ao universo, ainda não tenho certeza absoluta.',
+      'No meio da dificuldade encontra-se a oportunidade.',
+      'Não tentes ser bem-sucedido, tenta antes ser um homem de valor.',
+      'Se os fatos não se encaixam na teoria, modifique os fatos.'
+    ]
+  }
+
+  geraFrase = () => {
+   try {
+    let state = this.state;
+    let numeroFrase = Math.floor(Math.random() * this.frases.length);
+
+    state.textoFrase = this.frases[numeroFrase];
+    this.setState(state);
+   } catch (error) {
+     this.setState({error: error })
+   }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar bbackgroundColor={ Colors.light } barStyle="light-content" />
+
+        <Image  source={ img } style={ styles.img } />
+
+        <Botao cor="#FFF" onClick={ this.geraFrase } />
+
+        <Text style={styles.textoFrase}>{ this.state.textoFrase }</Text>
+      </View>
+    )
+  }
+}
+
+export default App;
