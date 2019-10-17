cb960aa616906f8573cc305a50801e073fd0c632
Desenvolvedoracm <desenvolvedoracm@gmail.com>
2019-10-17 00:44:09 -0300

    add

---
+++ AppTabNavigation/src/pages/pagina1.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class pagina1 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <View>
+                <Text> pagina1 </Text>
+            </View>
+        );
+    }
+}
+
+export default pagina1;
