19e7062961897e23043f52f2621ab70c283faac2
Desenvolvedoracm <desenvolvedoracm@gmail.com>
2019-10-30 15:43:53 -0300

    ajustes de icone e layout

--- AppConversorMoedas/src/index.js
+++ AppConversorMoedas/src/index.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import {
     StyleSheet,
     View,
-
+    Text
 } from 'react-native';
 import Conversor from './components/Conversor';
+import './config/StatusBarConfig';
 
 const styles = StyleSheet.create({
     container: {
@@ -27,6 +28,19 @@ export default class App extends Component {
     render() {
         return (
             <View style={styles.container}>
+                <View style={{ 
+                    width: '100%', 
+                    height: 70, 
+                    backgroundColor: '#7B68EE', 
+                    justifyContent: 'center', 
+                    alignItems: 'center'}}>
+                    <Text style={{ 
+                        color: '#FFF', 
+                        textAlign: 'center', 
+                        fontSize: 25,
+                        justifyContent: 'center',
+                        alignItems: 'center'}}>Conversor moedas</Text>
+                </View>
                 <Conversor moedaA="USD" moedaB="BRL" />
                 <Conversor moedaA="EUR" moedaB="BRL"  />
             </View>
