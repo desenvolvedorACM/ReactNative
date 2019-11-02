c563db8742970b2895ea504843fa758b17ba895c
Desenvolvedoracm <desenvolvedoracm@gmail.com>
2019-11-02 17:04:59 -0300

    alterações

---
+++ ReactMaps/src/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> teste </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
