0a88127c7d762e55c72c276ff486327d8fb32686
Desenvolvedoracm <desenvolvedoracm@gmail.com>
2020-02-11 12:23:41 -0300

    ajustes

--- SectionList/src/index.js
+++ SectionList/src/index.js
@@ -8,29 +8,12 @@ import '../src/config/StatusBarConfig';
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        paddingTop: 22
-    },
-    sectionContainer: {
-        flex: 1,
-        height: 80,
-        borderWidth: 1,
-        borderColor: '#808080',
-        backgroundColor: '#DDD',
-        borderRadius: 5,
-        margin: 10,
-        padding: 10,
-        alignItems: 'flex-start',
-        justifyContent: 'flex-start'
-    },
-    sectionText: {
-        fontSize: 14,
-        color: '#000'
+        flex: 1
     },
     sectionHeaderText: {
         fontSize: 14,
         fontWeight: 'bold',
-        color: '#000'
+        color: '#FFF'
     },
     sectionHeader: {
         backgroundColor: '#8055FF',
@@ -38,25 +21,30 @@ const styles = StyleSheet.create({
         height: 50,
         justifyContent: 'center'
     },
-    sectionHeaderTitle: {
-        fontSize: 14,
+    sectionHeaderTitleText: {
+        fontSize: 15,
         fontWeight: 'bold',
-        color: '#000',
-        backgroundColor: '#EEEE55',
-        paddingTop: 10,
-        paddingLeft: 10,
-        paddingRight: 10,
-        paddingBottom: 2
+        color: '#FFF'
+    },
+    sectionHeaderTitle: {
+        backgroundColor: '#5566EE',
+        padding: 15
+    },
+    itemRow: {
+        padding: 5,
+        height: 50,
+        fontSize: 15
     }
 });
 
 
 const {
     container,
-    sectionContainer,
-    sectionText,
     sectionHeader,
-    sectionHeaderText
+    sectionHeaderText,
+    sectionHeaderTitle,
+    sectionHeaderTitleText,
+    itemRow
 } = styles;
 
 
@@ -64,68 +52,30 @@ export default class App extends Component {
     constructor(props) {
         super(props);
 
-
         this.state = {
-            listaAgenda: [
+            avangers: [
                 {
-                    title: 'Alexandre Marques',
-                    data: [
-                        {
-                            id: 1,
-                            nome: 'Alexandre',
-                            email: 'teste@teste.com'
-                        },
-                        {
-                            id: 1,
-                            nome: 'Alexandre',
-                            email: 'teste@teste.com'
-                        }
-                    ]
+                    title: 'Heróis',
+                    data: ['Homem de ferro', 'Hulk', 'Thor', 'Capitão America', 'Homem aranha']
                 },
                 {
-                    title: 'Daiane Marques',
-                    data: [
-                        {
-                            id: 1,
-                            nome: 'Angelita da silva',
-                            email: 'teste@teste.com'
-                        },
-                        {
-                            id: 1,
-                            nome: 'Ilse de oliveria',
-                            email: 'teste@teste.com'
-                        }
-                    ]
-                },
-                {
-                    title: 'Vanessa Silva',
-                    data: [
-                        {
-                            id: 1,
-                            nome: 'Angelita da silva',
-                            email: 'teste@teste.com'
-                        },
-                        {
-                            id: 1,
-                            nome: 'Ilse de oliveria',
-                            email: 'teste@teste.com'
-                        }
-                    ]
+                    title: 'Vilões',
+                    data: ['Loki', 'Ultron', 'Thanos', 'Venom', 'Duende verde']
                 }
             ]
         }
     }
 
 
-    renderItemHeader = ({ item }) => (
-        <Text style={sectionHeaderTitle}>Titulo: {item.title}</Text>
+    renderItemHeader = (section) => (
+        <View style={sectionHeaderTitle}>
+            <Text style={sectionHeaderTitleText}>{section.title}</Text>
+        </View>
     )
 
-    renderItem = ({ item }) => (
-        <View style={sectionContainer}>
-            <Text style={sectionText}>NOME: {item.nome}</Text>
-            <Text style={sectionText}>EMAIL: {item.email}</Text>
-        </View>
+
+    renderItem = (item) => (
+        <Text style={itemRow}>{item}</Text>
     )
 
     render() {
@@ -133,13 +83,13 @@ export default class App extends Component {
             <View style={container}>
 
                 <View style={sectionHeader}>
-                    <Text style={sectionHeaderText}>SECTIONLIST</Text>
+                    <Text style={sectionHeaderText}>AVANGERS</Text>
                 </View>
 
                 <SectionList
-                    sections={this.state.listaAgenda}
+                    sections={this.state.avangers}
                     renderItem={({ item }) => this.renderItem(item)}
-                    renderSectionHeader={({ section }) => this.renderItemHeader(item)}
+                    renderSectionHeader={({ section }) => this.renderItemHeader(section)}
                     keyExtractor={(item, index) => index} />
             </View>
         );
